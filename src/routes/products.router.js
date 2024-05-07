e798aad3e00996c3a82f676a0b529880006bc946
Bruno Alberstein <albersteinbruno@gmail.com>
2024-05-07 13:44:12 -0300

    Corrección de ubicación de get de products de views.router a products.router

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,26 +1,68 @@
 const express = require("express");
 const router = express.Router();
-
 const ProductManager = require("../controllers/product-manager-db.js");
+const ProductModel = require("../models/product.model");
+
 const productManager = new ProductManager();
 
 router.get("/products", async (req, res) => {
-    try {
-        const limit = req.query.limit;
-        const productos = await productManager.getProducts();
-        if (limit) {
-            res.json(productos.slice(0, limit));
-        } else {
-            res.json(productos);
-        }
-    } catch (error) {
+    let { limit = 10, page = 1, sort, query } = req.query;
 
-        console.error("Error al obtener productos", error);
-        res.status(500).json({
-            error: "Error interno del servidor"
-        });
+    limit = parseInt(limit);
+    page = parseInt(page);
+
+    // Definir el objeto de opciones de paginación
+    const options = {
+        limit,
+        page,
+    };
+
+    // Verificar si se proporcionó un criterio de búsqueda (query)
+    const filter = query ? { category: query } : {};
+
+    // Aplicar el criterio de ordenamiento si se proporcionó
+    if (sort === 'asc') {
+        options.sort = { price: 1 }; // Orden ascendente por precio
+    } else if (sort === 'desc') {
+        options.sort = { price: -1 }; // Orden descendente por precio
     }
-})
+
+    try {
+        const categories = await ProductModel.distinct("category");
+        const productos = await ProductModel.paginate(filter, options);
+
+        const productosResultadoFinal = productos.docs.map(producto => {
+            const {_id, ...rest} = producto.toObject();
+            return rest;
+        });
+
+        // Calcular los enlaces directos a las páginas previas y siguientes, manteniendo los parámetros de consulta y orden
+        const prevLink = productos.hasPrevPage ? `/api/products?limit=${limit}&page=${productos.prevPage}&sort=${sort}&query=${query}` : null;
+        const nextLink = productos.hasNextPage ? `/api/products?limit=${limit}&page=${productos.nextPage}&sort=${sort}&query=${query}` : null;
+
+        res.render("cartelera", { 
+            status: 'success',
+            payload: {
+                productos: productosResultadoFinal,
+                pagination: {
+                    hasPrevPage: productos.hasPrevPage,
+                    hasNextPage: productos.hasNextPage,
+                    prevPage: productos.prevPage,
+                    nextPage: productos.nextPage,
+                    currentPage: productos.page,
+                    totalPages: productos.totalPages,
+                    prevLink,
+                    nextLink
+                },
+                categories: categories
+            },
+            session: req.session
+        });
+    } catch (error) {
+        console.error("Error al obtener productos:", error);
+        res.status(500).json({ status: 'error', message: "Error interno del servidor" });
+    }
+});
 
 router.get("/products/:pid", async (req, res) => {
 
@@ -42,7 +84,7 @@ router.get("/products/:pid", async (req, res) => {
             error: "Error interno del servidor"
         });
     }
-})
+});
 
 router.post("/products", async (req, res) => {
     const nuevoProducto = req.body; 
@@ -53,7 +95,7 @@ router.post("/products", async (req, res) => {
     } catch (error) {
         res.status(500).json({error: "Error interno del servidor"});
     }
-})
+});
 
 router.put("/products/:pid", async (req, res) => {
     const id = req.params.pid;
@@ -67,7 +109,7 @@ router.put("/products/:pid", async (req, res) => {
     } catch (error) {
         res.status(500).json({error: "Error interno del servidor"});
     }
-})
+});
 
 router.delete("/products/:pid", async (req, res) => {
     const id = req.params.pid; 
@@ -80,5 +122,6 @@ router.delete("/products/:pid", async (req, res) => {
     } catch (error) {
         res.status(500).json({error: "Error interno del servidor"});
     }
-})
+});
+
 module.exports = router;
--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,68 +1,8 @@
 const express = require("express");
-const router = express.Router(); 
-const ProductModel = require("../models/product.model");
+const router = express.Router();
 
 router.get("/", (req, res) => {
     res.render("home", { session: req.session });
-});
-
-router.get("/products", async (req, res) => {
-    let { limit = 10, page = 1, sort, query } = req.query;
-
-    limit = parseInt(limit);
-    page = parseInt(page);
-
-    // Definir el objeto de opciones de paginación
-    const options = {
-        limit,
-        page,
-    };
-
-    // Verificar si se proporcionó un criterio de búsqueda (query)
-    const filter = query ? { category: query } : {};
-
-    // Aplicar el criterio de ordenamiento si se proporcionó
-    if (sort === 'asc') {
-        options.sort = { price: 1 }; // Orden ascendente por precio
-    } else if (sort === 'desc') {
-        options.sort = { price: -1 }; // Orden descendente por precio
-    }
-
-    try {
-        const categories = await ProductModel.distinct("category");
-        const productos = await ProductModel.paginate(filter, options);
-
-        const productosResultadoFinal = productos.docs.map(producto => {
-            const {_id, ...rest} = producto.toObject();
-            return rest;
-        });
-
-        // Calcular los enlaces directos a las páginas previas y siguientes, manteniendo los parámetros de consulta y orden
-        const prevLink = productos.hasPrevPage ? `/products?limit=${limit}&page=${productos.prevPage}&sort=${sort}&query=${query}` : null;
-        const nextLink = productos.hasNextPage ? `/products?limit=${limit}&page=${productos.nextPage}&sort=${sort}&query=${query}` : null;
-
-        res.render("cartelera", { 
-            status: 'success',
-            payload: {
-                productos: productosResultadoFinal,
-                pagination: {
-                    hasPrevPage: productos.hasPrevPage,
-                    hasNextPage: productos.hasNextPage,
-                    prevPage: productos.prevPage,
-                    nextPage: productos.nextPage,
-                    currentPage: productos.page,
-                    totalPages: productos.totalPages,
-                    prevLink,
-                    nextLink
-                },
-                categories: categories
-            },
-            session: req.session
-        });
-    } catch (error) {
-        console.error("Error al obtener productos:", error);
-        res.status(500).json({ status: 'error', message: "Error interno del servidor" });
-    }
 });
 
 router.get("/realtimeproducts", (req, res) => {
